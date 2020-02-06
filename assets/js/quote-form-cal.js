8c648901757cdd4e80e66129fdc70da72810c2ee
maximeparent <60401437+maximeparent@users.noreply.github.com>
2020-02-06 15:20:59 -0500

    Test commit 4

--- assets/js/quote-form-cal.js
+++ assets/js/quote-form-cal.js
@@ -1,80 +1,3 @@
-//To find the number of elevator the user needs for the option RESIDENTIAL.
-$(document).ready(function () {
-	$(".option-res").change(function () {
-		var $nbrAppartement = $("#nbrAppartement").val()
-		var $nbrFloor = $("#nbrFloor").val();
-		var $nbrBasement = $("#nbrBasement").val();
-
-		var $nbrDoorsPerFloor = Math.ceil($nbrAppartement / ($nbrFloor * 6));
-
-		var $column = Math.ceil($nbrFloor / 20);
-
-		var nbrElevators = parseFloat($nbrDoorsPerFloor) * parseFloat($column);
-
-		//var nbrElevators will tell how much elevators the user needs and then it will show the result in input id="resultElevator"
-		$("#resultElevator").val(nbrElevators);
-	})
-});
-
-//To find the number of elevator the user needs for the option COMMERCIAL
-$(document).ready(function () {
-	$(".option-com").change(function () {
-		var $nbrBusinessCom = $("#nbrBusinessCom").val()
-		var $nbrFloorCom = $("#nbrFloorCom").val();
-		var $nbrBasementCom = $("#nbrBasementCom").val();
-		var $nbrParkingCom = $("#nbrParkingCom").val();
-		var $nbrElevatorCom = $("#nbrElevatorCom").val();
-
-		//var $nbrElevatorCom will tell how much elevators the user needs and then it will show the result in input id="resultElevator"
-		$("#resultElevator").val($nbrElevatorCom);
-	})
-});
-
-
-
-//To find the number of elevator the user needs for the option CORPORATE
-$(document).ready(function () {
-	$(".option-cor").change(function () {
-		var $nbrEcompaniesCor = $("#nbrEcompaniesCor").val()
-		var $nbrFloorCor = $("#nbrFloorCor").val()
-		var $nbrBasementCor = $("#nbrBasementCor").val()
-		var $nbrParkingCor = $("#nbrParkingCor").val()
-		var $nbrOccupantsPerFloorCor = $("#nbrOccupantsPerFloorCor").val()
-
-		var $nbrElevatorCor = Math.ceil($nbrOccupantsPerFloorCor * ($nbrFloorCor + $nbrBasementCor) / 1000);
-		var $columnCor = Math.ceil(($nbrFloorCor + $nbrBasementCor) / 20);
-		var $nbrResultElevatorsByCol = Math.ceil($nbrElevatorCor / $columnCor);
-		var $nberElevator = $nbrResultElevatorsByCol * $columnCor;
-
-		//var $nbrElevator will tell how much elevators the user needs and then it will show the result in input id="resultElevator"
-		$("#resultElevator").val($nberElevator);
-
-	})
-});
-
-//To find the number of elevator the user needs for the option HYBRID	
-$(document).ready(function () {
-	$(".option-hyb").change(function () {
-		var $nbrNumberOfDistinctBusinessesHyb = $("#nbrNumberOfDistinctBusinessesHyb").val()
-		var $nbrFloorHyb = $("#nbrFloorHyb").val()
-		var $nbrBasementHyb = $("#nbrBasementHyb").val()
-		var $nbrParkingHyb = $("#nbrParkingHyb").val()
-		var $nbrOccupantsPerFloorHyb = $("#nbrOccupantsPerFloorHyb").val()
-		var $nbrHoursOfActivityHyb = $("#nbrHoursOfActivityHyb").val()
-
-		var $nbrElevatorHyb = Math.ceil($nbrOccupantsPerFloorHyb * ($nbrFloorHyb + $nbrBasementHyb) / 1000);
-
-		var $columnHyb = Math.ceil(($nbrFloorHyb + $nbrBasementHyb) / 20);
-
-		var $nbrResultElevatorsByColHyb = Math.ceil($nbrElevatorHyb / $columnHyb);
-
-		var $nbrElevator = $nbrResultElevatorsByColHyb * $columnHyb;
-
-		//var $nbrElevator tell how much elevators the user needs and then it will show the result in input id="resultElevator"
-		$("#resultElevator").val($nbrElevator);
-	})
-});
-
 function standard() {
 	var $resultElevatorSt = parseInt($("#resultElevator").val())
 	$("#priceElevator").val(7565)
@@ -112,4 +35,130 @@ function excelium() {
 	$("#priceInstalation").val($installationCostsEx);
 	// var $resultEx tell the total cost and then it will show the result in input id="priceTotal"
 	$("#priceTotal").val($resultEx)
-}
+}	
+
+	//To find the number of elevator the user needs for the option RESIDENTIAL.
+$(document).ready(function () {
+	$(".option-res").change(function () {
+		var $nbrAppartement = $("#nbrAppartement").val()
+		var $nbrFloor = $("#nbrFloor").val();
+		var $nbrBasement = $("#nbrBasement").val();
+
+		var $nbrDoorsPerFloor = Math.ceil($nbrAppartement / ($nbrFloor * 6));
+
+		var $column = Math.ceil($nbrFloor / 20);
+
+		var nbrElevators = parseFloat($nbrDoorsPerFloor) * parseFloat($column);
+
+		if ($("#standard").is(":checked")) {
+			standard();
+		}
+
+		else if ($("#prenium").is(":checked")) {
+			premium();
+		}
+
+		else if ($("#excelium").is(":checked")) {
+			excelium();
+		}
+		
+		//var nbrElevators will tell how much elevators the user needs and then it will show the result in input id="resultElevator"
+		$("#resultElevator").val(nbrElevators);
+	})
+});
+
+
+//To find the number of elevator the user needs for the option COMMERCIAL
+$(document).ready(function () {
+	$(".option-com").change(function () {
+		var $nbrBusinessCom = $("#nbrBusinessCom").val()
+		var $nbrFloorCom = $("#nbrFloorCom").val();
+		var $nbrBasementCom = $("#nbrBasementCom").val();
+		var $nbrParkingCom = $("#nbrParkingCom").val();
+		var $nbrElevatorCom = $("#nbrElevatorCom").val();
+
+		if ($("#standard").is(":checked")) {
+			standard();
+		}
+
+		else if ($("#prenium").is(":checked")) {
+			premium();
+		}
+
+		else if ($("#excelium").is(":checked")) {
+			excelium();
+		}
+
+		//var $nbrElevatorCom will tell how much elevators the user needs and then it will show the result in input id="resultElevator"
+		$("#resultElevator").val($nbrElevatorCom);
+	})
+});
+
+
+
+//To find the number of elevator the user needs for the option CORPORATE
+$(document).ready(function () {
+	$(".option-cor").change(function () {
+		var $nbrEcompaniesCor = $("#nbrEcompaniesCor").val()
+		var $nbrFloorCor = $("#nbrFloorCor").val()
+		var $nbrBasementCor = $("#nbrBasementCor").val()
+		var $nbrParkingCor = $("#nbrParkingCor").val()
+		var $nbrOccupantsPerFloorCor = $("#nbrOccupantsPerFloorCor").val()
+
+		var $nbrElevatorCor = Math.ceil($nbrOccupantsPerFloorCor * ($nbrFloorCor + $nbrBasementCor) / 1000);
+		var $columnCor = Math.ceil(($nbrFloorCor + $nbrBasementCor) / 20);
+		var $nbrResultElevatorsByCol = Math.ceil($nbrElevatorCor / $columnCor);
+		var $nberElevator = $nbrResultElevatorsByCol * $columnCor;
+
+		if ($("#standard").is(":checked")) {
+			standard();
+		}
+
+		else if ($("#prenium").is(":checked")) {
+			premium();
+		}
+
+		else if ($("#excelium").is(":checked")) {
+			excelium();
+		}
+
+		//var $nbrElevator will tell how much elevators the user needs and then it will show the result in input id="resultElevator"
+		$("#resultElevator").val($nberElevator);
+
+	})
+});
+
+//To find the number of elevator the user needs for the option HYBRID	
+$(document).ready(function () {
+	$(".option-hyb").change(function () {
+		var $nbrNumberOfDistinctBusinessesHyb = $("#nbrNumberOfDistinctBusinessesHyb").val()
+		var $nbrFloorHyb = $("#nbrFloorHyb").val()
+		var $nbrBasementHyb = $("#nbrBasementHyb").val()
+		var $nbrParkingHyb = $("#nbrParkingHyb").val()
+		var $nbrOccupantsPerFloorHyb = $("#nbrOccupantsPerFloorHyb").val()
+		var $nbrHoursOfActivityHyb = $("#nbrHoursOfActivityHyb").val()
+
+		var $nbrElevatorHyb = Math.ceil($nbrOccupantsPerFloorHyb * ($nbrFloorHyb + $nbrBasementHyb) / 1000);
+
+		var $columnHyb = Math.ceil(($nbrFloorHyb + $nbrBasementHyb) / 20);
+
+		var $nbrResultElevatorsByColHyb = Math.ceil($nbrElevatorHyb / $columnHyb);
+
+		var $nbrElevator = $nbrResultElevatorsByColHyb * $columnHyb;
+
+		if ($("#standard").is(":checked")) {
+			standard();
+		}
+
+		else if ($("#prenium").is(":checked")) {
+			premium();
+		}
+
+		else if ($("#excelium").is(":checked")) {
+			excelium();
+		}
+
+		//var $nbrElevator tell how much elevators the user needs and then it will show the result in input id="resultElevator"
+		$("#resultElevator").val($nbrElevator);
+	})
+});
