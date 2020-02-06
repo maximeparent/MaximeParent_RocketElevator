cf6e075f4e46177df442462295b66081a9f53bf5
maximeparent <60401437+maximeparent@users.noreply.github.com>
2020-02-06 11:20:05 -0500

    test git VSCode

--- assets/js/quote-form.js
+++ assets/js/quote-form.js
@@ -1,39 +1,40 @@
-$(document).ready(function(){
+// When user load the webpage every questions is hidden.
+
+$(document).ready(function () {
 	$(".option-res").hide();
 	$(".option-com").hide();
 	$(".option-cor").hide();
 	$(".option-hyb").hide();
 });
 
-$("#form-answer").change(function() {
+// When user choose an option show the right elements and hide the rest.
+$("#form-answer").change(function () {
 	var value = $(this).val();
-		if (value === "residential") {
-			$(".option-res").show();
-			$(".option-com").hide();
-			$(".option-cor").hide();
-			$(".option-hyb").hide();
-		} else if (value === "commercial") { 
-			$(".option-res").hide();
-			$(".option-com").show();
-			$(".option-cor").hide();
-			$(".option-hyb").hide();
-		} else if (value === "corporate") {
-			$(".option-res").hide();
-			$(".option-com").hide();
-			$(".option-cor").show();
-			$(".option-hyb").hide();
-		} else if (value === "hybrid") {
-			$(".option-res").hide();
-			$(".option-com").hide();
-			$(".option-cor").hide();
-			$(".option-hyb").show();
-		} else {
-			$(".option-res").hide();
-			$(".option-com").hide();
-			$(".option-cor").hide();
-			$(".option-hyb").hide();
-			}
+	if (value === "residential") {
+		$(".option-res").show();
+		$(".option-com").hide();
+		$(".option-cor").hide();
+		$(".option-hyb").hide();
+	} else if (value === "commercial") {
+		$(".option-res").hide();
+		$(".option-com").show();
+		$(".option-cor").hide();
+		$(".option-hyb").hide();
+	} else if (value === "corporate") {
+		$(".option-res").hide();
+		$(".option-com").hide();
+		$(".option-cor").show();
+		$(".option-hyb").hide();
+	} else if (value === "hybrid") {
+		$(".option-res").hide();
+		$(".option-com").hide();
+		$(".option-cor").hide();
+		$(".option-hyb").show();
+	} else {
+		$(".option-res").hide();
+		$(".option-com").hide();
+		$(".option-cor").hide();
+		$(".option-hyb").hide();
+	}
 
-		});
-
-
+});
